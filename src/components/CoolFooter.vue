2e581c6de2c6ab3e3430bae3239bd0497ac2c54c
Samuel <samuellemosbentosilva@gmail.com>
2024-05-17 21:53:11 -0300

    Create Footer

--- src/components/CoolFooter.vue
+++ src/components/CoolFooter.vue
@@ -2,14 +2,29 @@
 <template>
     
 
-<footer class="d-flex bg-dark  flex-wrap  py-3 my-4 " >
-<div class="d-flex w-75 align-items-end justify-content-end">
-    <p class="d-flex mb-0 text-light ">MyPiano</p>
+<footer class="d-flex bg-dark  flex-wrap p-0 mt-5 " >
+    <div class="d-flex w-25 align-items-center justify-content-center">
+   
 </div>
-    <nav class=" d-flex w-25 justify-content-start">
-    <router-link><img src="../assets/SocialNetworks/icons8-github.gif" alt="GitHub"></router-link>
-    <router-link><img src="../assets/SocialNetworks/icons8-instagram.gif" alt=""></router-link>
-    
+    <div class="d-flex flex-column w-50 align-items-center justify-content-center">
+    <img src="../assets/icons8-electronic-music-64.png" class="rounded ml-5 " alt="">
+    <p class="text-light"><strong>MyPiano</strong></p>
+</div>
+
+    <nav class=" d-flex w-25 justify-content-end space-between  flex-rown ">
+    <div class="d-flex flex-column mr-4 ml-2 p-3 align-items-center" >
+        <a href="http://" class="pr-2 pl-2">
+            <img class="rounded-circle " src="../assets/SocialNetworks/icons8-github.gif" alt="GitHub">
+        <p class="text-light"><strong>Github</strong></p>
+        </a>
+    </div>
+    <div class="d-flex flex-column mr-2 ml-4 p-3 align-items-center" >
+        <a href="http://" class="pr-2 pl-2">
+            <img class="rounded-circle anim" src="../assets/SocialNetworks/icons8-instagram.svg" alt="GitHub">
+        <p class="text-light"><strong>Instagram</strong></p>
+        </a>
+    </div>
+  
   </nav>
 </footer>
 
@@ -20,3 +35,31 @@
 export default {        
 }
     </script>
+
+    <style>
+*{
+    text-decoration: none !important;
+}
+.anim{
+    transition: transform 0.3s;
+
+}
+@keyframes pulse {
+        0% {
+            transform: scale(1.0);
+        }
+        50% {
+            transform: scale(1.1);
+        }
+        100% {
+            transform: scale(1.0);
+        }
+    }
+    .anim{
+        animation: pulse 2s infinite;
+    }
+img:hover{
+    animation:none;
+    transform: scale(1.1);
+}
+</style>
